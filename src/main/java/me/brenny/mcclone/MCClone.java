32bbb14d8d73c1f50deefa43f6ae14ce0bc2e561
1Brenny1 <brendanrcg@gmail.com>
2024-10-27 16:09:50 -0400

    Fix stupid issue

--- src/main/java/me/brenny/mcclone/MCClone.java
+++ src/main/java/me/brenny/mcclone/MCClone.java
@@ -12,14 +12,13 @@ public class MCClone {
     public static final Vec2 DEFAULT_SCREEN_SIZE = new Vec2(1080, 720);
     public static final int BLOCK_SIZE = 100;
 
-    public static Image TEXTURE = new Image("terrain.png");
     public static PhongMaterial MATERIAL = new PhongMaterial();
 
     public static void main(String[] args) {
 
-        //MATERIAL.setDiffuseColor(Color.RED);
+        MATERIAL.setDiffuseColor(Color.RED);
 
-        //ChunkManager.init();
+        ChunkManager.init();
 
         Window window = new Window();
         window.init(args);
